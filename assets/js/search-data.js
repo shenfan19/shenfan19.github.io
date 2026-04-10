70b58095f4d2736d13a3dff4bc127eea8ead84e6
shenfan19 <shenfan19@gmail.com>
2026-04-10 09:46:40 +0000

    Deploying to gh-pages from @ shenfan19/shenfan19.github.io@d539a2047e0f1c7d9d8b3734e16ca7ddf805a2fb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -387,13 +387,13 @@ ninja.data = [{
           section: "News",},{id: "news-joined-sun-yat-sen-university-as-vice-researcher-at-the-school-of-system-science-and-engineering",
           title: 'Joined Sun Yat-Sen University as Vice Researcher at the School of System Science...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-solar-sail-structual-design",
+          title: 'Solar Sail Structual design',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-structural-optimization-for-nacelle-on-civil-aircraft",
+          title: 'Structural optimization for nacelle on civil aircraft',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
@@ -402,36 +402,6 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project.html";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project.html";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project.html";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project.html";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project.html";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project.html";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project.html";
             },},{
         id: 'social-cv',
         title: 'CV',
